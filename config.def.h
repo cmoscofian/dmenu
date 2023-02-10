55575dffa7eb2404845a0e81a9e75f6717c9e886
Charles Moscofian <charlesmoscofian@hotmail.com>
2023-02-10 18:39:28 +0100

    feat: nibble colors and patching
    Add nibble colors, centered and fuzzy finding patches and a few remaps
    to make it even more awesome.

--- config.def.h
+++ config.def.h
@@ -1,20 +1,42 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar           = 1;            /* -b option; if 0, dmenu appears at bottom */
+static int centered         = 1;            /* -c option; centers dmenu on screen */
+static unsigned int lines   = 10;           /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static const char *prompt   = NULL;         /* -p option; prompt to the left of input field */
+
+static int min_width                = 500;  /* minimum width when centered */
+static unsigned int border_width    = 1;    /* size of the window border */
+
+
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-	"monospace:size=10"
+static const char *fonts[] = { "Unifont:size=15", "monospace:size=10" };
+
+/* nibble color scheme */
+static const char black[]           = "#000000";
+static const char white[]           = "#ffffff";
+static const char dark_red[]        = "#800000";
+static const char dark_green[]      = "#008000";
+static const char dark_yellow[]     = "#808000";
+static const char dark_blue[]       = "#000080";
+static const char dark_fuchsia[]    = "#800080";
+static const char dark_cyan[]       = "#008080";
+static const char dark_grey[]       = "#404040";
+static const char light_red[]       = "#ff0000";
+static const char light_green[]     = "#00ff00";
+static const char light_yellow[]    = "#ffff00";
+static const char light_blue[]      = "#0000ff";
+static const char light_fuchsia[]   = "#ff00ff";
+static const char light_cyan[]      = "#00ffff";
+static const char light_grey[]      = "#c0c0c0";
+
+static const char *colors[SchemeLast][3] = {
+	/*                  fg          bg          border      */
+	[SchemeNorm]    = { dark_blue,  light_grey, light_cyan  },
+	[SchemeSel]     = { light_grey, dark_cyan,  light_cyan  },
+	[SchemeOut]     = { black,      white,      light_cyan  },
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
-};
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
 
 /*
  * Characters not considered part of a word while deleting words
