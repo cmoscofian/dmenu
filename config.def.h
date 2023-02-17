b7ac70fb031e5af1517c512316d101e2ba8d5533
Charles Moscofian <charlesmoscofian@hotmail.com>
2023-02-17 11:47:56 +0100

    feat(fuzzy): add fuzzy matching functionality
    Make dmenu accepts the `-z` flag which enables fuzzy matching, instead
    of regular continuous pattern matching.
    Also, make centered the default behaviour for dmenu, removing the `-c`
    flag option to control it.

--- config.def.h
+++ config.def.h
@@ -2,9 +2,9 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar           = 1;            /* -b option; if 0, dmenu appears at bottom */
-static int centered         = 1;            /* -c option; centers dmenu on screen */
 static unsigned int lines   = 10;           /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static const char *prompt   = NULL;         /* -p option; prompt to the left of input field */
+static int fuzzy            = 0;            /* -z option; if 0, dmenu doesn't use fuzzy matching */
 
 static int min_width                = 500;  /* minimum width when centered */
 static unsigned int border_width    = 1;    /* size of the window border */
