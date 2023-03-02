0e0a9fc1cdc243f7a557974e13fcfc92aa927472
Charles Moscofian <charlesmoscofian@hotmail.com>
2023-03-03 00:21:44 +0100

    feat(topbar): remove topbar config
    Remove all logic attached to setting if the top should be displayed on
    the top of the root window or bottom, since the default is always
    centered for my fork.

--- config.def.h
+++ config.def.h
@@ -1,14 +1,12 @@
 /* See LICENSE file for copyright and license details. */
+
 /* Default settings; can be overriden by command line. */
+static unsigned int lines       = 10;       /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static const char *prompt       = NULL;     /* -p option; prompt to the left of input field */
+static int fuzzy                = 0;        /* -z option; if 0, dmenu doesn't use fuzzy matching */
 
-static int topbar           = 1;            /* -b option; if 0, dmenu appears at bottom */
-static unsigned int lines   = 10;           /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static const char *prompt   = NULL;         /* -p option; prompt to the left of input field */
-static int fuzzy            = 0;            /* -z option; if 0, dmenu doesn't use fuzzy matching */
-
-static int min_width                = 500;  /* minimum width when centered */
-static unsigned int border_width    = 1;    /* size of the window border */
-
+static int minwidth             = 500;      /* minimum width when centered */
+static unsigned int borderwidth = 3;        /* size of the window border */
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = { "Unifont:size=15", "monospace:size=10" };
@@ -31,14 +29,14 @@ static const char light_fuchsia[]   = "#ff00ff";
 static const char light_cyan[]      = "#00ffff";
 static const char light_grey[]      = "#c0c0c0";
 
+static const char *bordercolor      = dark_grey;
+
 static const char *colors[SchemeLast][ColLast] = {
 	/*                  fg          bg          */
 	[SchemeNorm]    = { dark_blue,  light_grey  },
 	[SchemeSel]     = { light_grey, dark_cyan   },
 	[SchemeOut]     = { black,      white       },
 };
-
-static const char *bordercolor = light_cyan;
 
 /*
  * Characters not considered part of a word while deleting words
