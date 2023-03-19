3eeb8d5bfe477f1d0b359e32bc0d4a233d7d7091
Charles Moscofian <charlesmoscofian@hotmail.com>
2023-03-19 23:41:11 +0100

    enhancement(ui): change borders
    Change border size and colour.

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@ static const char *prompt       = NULL;     /* -p option; prompt to the left of 
 static int fuzzy                = 0;        /* -z option; if 0, dmenu doesn't use fuzzy matching */
 
 static int minwidth             = 500;      /* minimum width when centered */
-static unsigned int borderwidth = 3;        /* size of the window border */
+static unsigned int borderwidth = 2;        /* size of the window border */
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = { "Unifont:size=15", "monospace:size=10" };
@@ -29,7 +29,7 @@ static const char light_fuchsia[]   = "#ff00ff";
 static const char light_cyan[]      = "#00ffff";
 static const char light_grey[]      = "#c0c0c0";
 
-static const char *bordercolor      = dark_grey;
+static const char *bordercolor      = dark_cyan;
 
 static const char *colors[SchemeLast][ColLast] = {
 	/*                  fg          bg          */
